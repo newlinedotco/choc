f6cee66327be7daef94bb2d43056a541d4d4f25e
Nate Murray <nate@natemurray.com>
2013-09-12 11:57:14 -0700

    reorganizing

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+'use strict';
+module.exports = function (grunt) {
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+		clean: {
+      dist: [
+				'js',
+      ],
+			test: [
+				'tmp',
+				'components',
+				'bower_components'
+			]
+		},
+
+    coffee: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'coffee/src',
+          src: '*.coffee',
+          dest: 'js/src',
+          ext: '.js'
+        }]
+      },
+      test: {
+        files: [{
+          expand: true,
+          cwd: 'coffee/test',
+          src: '*.coffee',
+          dest: 'js/test',
+          ext: '.js'
+        }]
+      }
+    },
+
+    wisp: {
+       default_options: {
+         options: {
+           srcBaseDir: "readable/src/"
+         },
+         files: {
+           'js/': ['readable/src/**/*.wisp']
+         }
+       }
+    },
+
+
+		bower: {
+			options: {
+				exclude: ['underscore']
+			},
+			standard: {
+				rjsConfig: 'tmp/config.js'
+			},
+			global: {
+				rjsConfig: 'tmp/global-config.js'
+			},
+			baseUrl: {
+				rjsConfig: 'tmp/baseurl-config.js'
+			}
+		}
+	});
+
+	// grunt.registerTask('bower-install', function () {
+	// 	require('bower').commands
+	// 		.install([
+	// 			'jquery',
+	// 			'underscore',
+	// 			'requirejs',
+	// 			'respond',
+	// 			'anima',
+	// 			'typeahead.js',
+	// 			'highstock'
+	// 		]).on('end', this.async());
+	// });
+
+    grunt.registerTask('build', [
+      'clean:dist',
+      'coffee'
+    ]);
+};
